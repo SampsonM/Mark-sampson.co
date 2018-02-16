f097e02bcb5a959f187f9df47e3e080ca6421b51
SampsonM <SampsonM@github.com>
2018-02-16 20:37:41 +0000

    CSS Grid update

--- js/about.js
+++ js/about.js
@@ -1,13 +1,9 @@
 function openNav() {
-    document.getElementById("menu").style.transition = "0.1s";
-    document.getElementById("mySidenav").style.width = "50px";
-    document.getElementById("main").style.marginLeft = "50px";
     document.getElementById("menu").style.visibility = "hidden";
+    document.getElementById("mySidenav").style.width = "50px";
 }
 
 function closeNav() {
-    document.getElementById("menu").style.transition = "0.2s";
     document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
     document.getElementById("menu").style.visibility = "visible";
 }
