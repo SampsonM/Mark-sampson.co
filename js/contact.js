e684228c42e6fb84456937750a53abc3fb980d4e
SampsonM <SampsonM@github.com>
2018-02-17 00:05:51 +0000

    etch update

--- js/contact.js
+++ js/contact.js
@@ -15,3 +15,56 @@ function emptyName() {
 function emptyEmail() {
     document.getElementById("email").value = "";
 }
+
+let txt = "Let's build something!";
+let i = 0;
+
+function typeWriter() {
+    if (i < txt.length) {
+        document.getElementById("text").innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(typeWriter, 80);
+    };
+};
+
+//etch sketch code
+const canvas = document.querySelector('#myCanvas');
+const ctx = canvas.getContext('2d');
+let x = 0;
+let y = 0;
+window.addEventListener("keydown", moveSomething, false);
+canvas.addEventListener("click", start, false);
+
+//finds start user position and sets [tested]
+function start(e) {
+    x = e.clientX;
+    y = e.clientY;
+    ctx.moveTo(x, y);
+    console.log(x, y);
+}
+
+function moveCtx(x, y) {
+    ctx.fillRect(x, y, 2, 2);
+    console.log(x, y);
+}
+
+function moveSomething(e) {
+    switch(e.keyCode) {
+        case 37: //left
+            x -= 2;
+            moveCtx(x, y);
+            break;
+        case 38: //up
+            y -= 2;
+            moveCtx(x, y);            
+            break;
+        case 39: //right
+            x += 2;
+            moveCtx(x, y);            
+            break;
+        case 40: //down
+            y += 2;
+            moveCtx(x, y);            
+            break;  
+    }   
+}
