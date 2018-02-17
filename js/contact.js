e0f0fce36ebdfcc3485f2a1bc2e241bf9b7c7233
SampsonM <SampsonM@github.com>
2018-02-17 15:54:22 +0000

    etch update

--- js/contact.js
+++ js/contact.js
@@ -1,20 +1,20 @@
 function openNav() {
     document.getElementById("mySidenav").style.width = "50px";
     document.getElementById("menu").style.visibility = "hidden";
-}
+};
 
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("menu").style.visibility = "visible";
-}
+};
 
 function emptyName() {
     document.getElementById("name").value = "";
-}
+};
 
 function emptyEmail() {
     document.getElementById("email").value = "";
-}
+};
 
 let txt = "Let's build something!";
 let i = 0;
@@ -30,41 +30,57 @@ function typeWriter() {
 //etch sketch code
 const canvas = document.querySelector('#myCanvas');
 const ctx = canvas.getContext('2d');
+const etchSketch = document.querySelector('#etch-sketch');
+const clear = document.querySelector('#clear');
 let x = 0;
 let y = 0;
+clear.addEventListener("click", reset, false);
 window.addEventListener("keydown", moveSomething, false);
-canvas.addEventListener("click", start, false);
+init();
 
-//finds start user position and sets [tested]
-function start(e) {
-    x = e.clientX;
-    y = e.clientY;
-    ctx.moveTo(x, y);
-    console.log(x, y);
+function init() {
+    x = etchSketch.offsetWidth / 2;
+    y = (etchSketch.offsetHeight / 4) * 3;
 }
 
+//moves ctx on key press
 function moveCtx(x, y) {
-    ctx.fillRect(x, y, 2, 2);
+    ctx.fillRect(x, y, 4, 4);
     console.log(x, y);
-}
+};
 
+//on certain key press ctx moves
 function moveSomething(e) {
     switch(e.keyCode) {
         case 37: //left
-            x -= 2;
+            x -= 4;
             moveCtx(x, y);
             break;
         case 38: //up
-            y -= 2;
+            y -= 4;
             moveCtx(x, y);            
             break;
         case 39: //right
-            x += 2;
+            x += 4;
             moveCtx(x, y);            
             break;
         case 40: //down
-            y += 2;
+            y += 4;
             moveCtx(x, y);            
             break;  
     }   
+};
+
+fitToContainer(canvas);
+
+function fitToContainer(canvas){
+  // ...then set the internal size to match
+  canvas.width  = etchSketch.offsetWidth;
+  canvas.height = etchSketch.offsetHeight;
 }
+
+function reset() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    init();
+};
+
