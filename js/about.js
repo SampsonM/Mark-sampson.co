86e2b90782785361ba4ea3dbe557ab1104bca008
SampsonM <SampsonM@github.com>
2018-03-08 21:56:35 +0000

    refactored

--- js/about.js
+++ js/about.js
@@ -1,9 +1,9 @@
 function openNav() {
-    document.getElementById("menu").style.visibility = "hidden";
+    document.getElementById("menu").style.display = "none";
     document.getElementById("mySidenav").style.width = "50px";
 }
 
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("menu").style.visibility = "visible";
+    document.getElementById("menu").style.display = "inline";
 }
